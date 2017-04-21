11aff5129672eb852f8c8fb7d0cb550f66e6e77e
NoeFiguet <noefiguet@hotmail.com>
2017-04-21 23:37:27 +0200

    Add files via upload

---
+++ new.py
@@ -0,0 +1,61 @@
+### C:\Python27\python.exe $(FULL_CURRENT_PATH)
+### C:\Python27\python -m pip install pydub
+import sys, time, os
+
+# try:
+ # time.sleep(1)
+ # python -m pip install ffmpy
+
+# except Exception as ex:
+    # print ex
+
+def remove(path):
+    """ param <path> could either be relative or absolute. """
+    if os.path.isfile(path):
+        os.remove(path)  # remove the file
+    elif os.path.isdir(path):
+        shutil.rmtree(path)  # remove dir and all contains
+    else:
+        print "file {} is not a file or dir.".format(path)
+
+
+def print_and_wait(text, wait):	
+	import time
+	print text
+	time.sleep(wait)
+
+
+def convert_mp3(input_file, baudrate):
+	print("Start convert_mp3")	
+	import os
+	output_folder = os.path.dirname(input_file) + '/out'
+	if not os.path.exists(output_folder):
+		os.makedirs(output_folder)
+	input_file_name = os.path.splitext(os.path.basename(input_file))[0]
+	output_folder = output_folder + '/' + input_file_name
+	print 'File will be placed in ' + output_folder
+	print 'bp'	
+	from pydub import AudioSegment
+	sound = AudioSegment.from_mp3(input_file)
+	sound.export(output_folder + ".mp3" , format="mp3", bitrate=baudrate)
+	
+def show_files_folder(path, extension):
+	print("Start show_files_folder")	
+	import os
+	for file in os.listdir(path):
+		if file.endswith(extension):
+			print(file)
+			convert_mp3(path + '/' + file, "128k")
+			print("Start Show Files")	
+		
+try:
+	#!/usr/share/python
+	import sys, time, os
+	file_location = os.getcwd()
+	print_and_wait("Start", 0.1)
+	show_files_folder(file_location, ".mp3")
+	print_and_wait("End", 1)
+except:
+	print "AudioSegment Error"
+	time.sleep(1)
+
